b9b0ddb9d077fa411270c35e6093bf78e5fa0de3
asmit27rai <raiasmit10@gmail.com>
2025-02-02 03:21:25 +0530

    Model for smart contract deployed on thirdweb on baseSepolia network

---
+++ riskmanager/hardhat.config.js
@@ -0,0 +1,33 @@
+require("@matterlabs/hardhat-zksync-solc");
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
